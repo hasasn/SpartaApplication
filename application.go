19b803370722eecee5603076a8d55a652178d515
Matt Weagle <mweagle@gmail.com>
2015-10-26 07:32:38 -0700

    Update handler signatures

--- application.go
+++ application.go
@@ -3,6 +3,7 @@ package main
 import (
 	logic "SpartaApplication/logic"
 	"fmt"
+	"github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	sparta "github.com/mweagle/Sparta"
@@ -11,13 +12,17 @@ import (
 
 const LAMBDA_EXECUTE_ROLE_NAME = "LambdaExecutor"
 
-func helloWorld(event sparta.LambdaEvent, context sparta.LambdaContext, w http.ResponseWriter) {
-	fmt.Fprintf(w, "Hello World! Data: %s", event)
+func helloWorld(event *sparta.LambdaEvent, context *sparta.LambdaContext, w *http.ResponseWriter, logger *logrus.Logger) {
+	logger.WithFields(logrus.Fields{
+		"RequestID": context.AWSRequestId,
+	}).Info("Hello world log message")
+
+	fmt.Fprintf(*w, "Hello World! Data: %s", event)
 }
 
 func sampleData() []*sparta.LambdaAWSInfo {
 	var lambdaFunctions []*sparta.LambdaAWSInfo
-	lambdaFn := sparta.NewLambda(LAMBDA_EXECUTE_ROLE_NAME, mockLambda1, nil)
+	lambdaFn := sparta.NewLambda(LAMBDA_EXECUTE_ROLE_NAME, helloWorld, nil)
 	lambdaFn.Permissions = append(lambdaFn.Permissions, &lambda.AddPermissionInput{
 		Action:      aws.String("lambda:InvokeFunction"),
 		Principal:   aws.String("s3.amazonaws.com"),
@@ -25,16 +30,17 @@ func sampleData() []*sparta.LambdaAWSInfo {
 		SourceArn:   aws.String("arn:aws:s3:::someBucket"),
 	})
 	lambdaFn.EventSourceMappings = append(lambdaFn.EventSourceMappings, &lambda.CreateEventSourceMappingInput{
-		EventSourceArn:   aws.String("arn:aws:dynamodb:us-west-2:123412341234:table/coconut-browswer/stream/2015-10-22T15:05:13.779"),
+		EventSourceArn:   aws.String("arn:aws:dynamodb:us-west-2:123412341234:table/tableName/stream/2015-10-22T15:05:13.779"),
 		StartingPosition: aws.String("TRIM_HORIZON"),
 		BatchSize:        aws.Int64(10),
 		Enabled:          aws.Bool(true),
 	})
 	lambdaFunctions = append(lambdaFunctions, lambdaFn)
-	lambdaFunctions = append(lambdaFunctions, sparta.NewLambda(LAMBDA_EXECUTE_ROLE_NAME, logic.LogicLambda1, nil))
+
+	lambdaFunctions = append(lambdaFunctions, sparta.NewLambda(LAMBDA_EXECUTE_ROLE_NAME, logic.LambdaLogic, nil))
 	return lambdaFunctions
 }
 
 func main() {
-	sparta.Main("Sparta Application", "This is a sample Sparta application", sampleData())
+	sparta.Main("SpartaApplication", "This is a sample Sparta application", sampleData())
 }
--- logic/logic_handler.go
+++ logic/logic_handler.go
@@ -2,10 +2,11 @@ package logic
 
 import (
 	"fmt"
+	"github.com/Sirupsen/logrus"
 	sparta "github.com/mweagle/Sparta"
 	"net/http"
 )
 
-func LambdaLogic(event sparta.LambdaEvent, context sparta.LambdaContext, w http.ResponseWriter) {
-	fmt.Fprintf(w, "LambdaLogic Data: %s", event)
+func LambdaLogic(event *sparta.LambdaEvent, context *sparta.LambdaContext, w *http.ResponseWriter, logger *logrus.Logger) {
+	fmt.Fprintf(*w, "LambdaLogic Data: %s", event)
 }
