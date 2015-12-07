4c11f5c17191668ecbb1955799fe41e94f03b69a
Matt Weagle <mweagle@gmail.com>
2015-12-07 08:37:47 -0800

    Unmarshal to Sparta Kinesis event

--- application.go
+++ application.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/lambda"
 	sparta "github.com/mweagle/Sparta"
 	spartaDynamoDB "github.com/mweagle/Sparta/aws/dynamodb"
+	spartaKinesis "github.com/mweagle/Sparta/aws/kinesis"
 	spartaSNS "github.com/mweagle/Sparta/aws/sns"
 )
 
@@ -26,6 +27,7 @@ func paramVal(keyName string, defaultValue string) string {
 var s3Bucket = paramVal("S3_TEST_BUCKET", "arn:aws:s3:::MyS3Bucket")
 var snsTopic = paramVal("SNS_TEST_TOPIC", "arn:aws:sns:us-west-2:123412341234:mySNSTopic")
 var dynamoTestStream = paramVal("DYNAMO_TEST_STREAM", "arn:aws:dynamodb:us-west-2:123412341234:table/myTableName/stream/2015-10-22T15:05:13.779")
+var kinesisTestStream = paramVal("KINESIS_TEST_STREAM", "arn:aws:kinesis:us-west-2:123412341234:stream/kinesisTestStream")
 
 ////////////////////////////////////////////////////////////////////////////////
 // S3 handler
@@ -123,6 +125,39 @@ func appendDynamoDBLambda(api *sparta.API, lambdaFunctions []*sparta.LambdaAWSIn
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Kinesis handler
+//
+func echoKinesisEvent(event *json.RawMessage, context *sparta.LambdaContext, w http.ResponseWriter, logger *logrus.Logger) {
+	logger.WithFields(logrus.Fields{
+		"RequestID": context.AWSRequestID,
+		"Event":     string(*event),
+	}).Info("Request received")
+
+	var lambdaEvent spartaKinesis.Event
+	err := json.Unmarshal([]byte(*event), &lambdaEvent)
+	if err != nil {
+		logger.Error("Failed to unmarshal event data: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	for _, eachRecord := range lambdaEvent.Records {
+		logger.WithFields(logrus.Fields{
+			"EventID": eachRecord.EventID,
+		}).Info("Kinesis Event")
+	}
+}
+
+func appendKinesisLambda(api *sparta.API, lambdaFunctions []*sparta.LambdaAWSInfo) []*sparta.LambdaAWSInfo {
+	lambdaFn := sparta.NewLambda(sparta.IAMRoleDefinition{}, echoKinesisEvent, nil)
+	lambdaFn.EventSourceMappings = append(lambdaFn.EventSourceMappings, &lambda.CreateEventSourceMappingInput{
+		EventSourceArn:   aws.String(kinesisTestStream),
+		StartingPosition: aws.String("TRIM_HORIZON"),
+		BatchSize:        aws.Int64(100),
+		Enabled:          aws.Bool(true),
+	})
+	return append(lambdaFunctions, lambdaFn)
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Return the *[]sparta.LambdaAWSInfo slice
 //
 func spartaLambdaData(api *sparta.API) []*sparta.LambdaAWSInfo {
@@ -131,6 +166,7 @@ func spartaLambdaData(api *sparta.API) []*sparta.LambdaAWSInfo {
 	lambdaFunctions = appendS3Lambda(api, lambdaFunctions)
 	lambdaFunctions = appendSNSLambda(api, lambdaFunctions)
 	lambdaFunctions = appendDynamoDBLambda(api, lambdaFunctions)
+	lambdaFunctions = appendKinesisLambda(api, lambdaFunctions)
 	return lambdaFunctions
 }
 
@@ -141,5 +177,5 @@ func main() {
 	sparta.Main(stackName,
 		"Simple Sparta application",
 		spartaLambdaData(apiGateway),
-		nil)
+		apiGateway)
 }
